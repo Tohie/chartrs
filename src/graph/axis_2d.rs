bed4829eef03502d3b422917819a298b5c5217b3
Tohie <scottdjwales@gmail.com>
2016-12-21 13:50:38 +0000

    Cleaned up draw_axis and moved pretty_axis_values out of Axis2D

--- src/graph/axis_2d.rs
+++ src/graph/axis_2d.rs
@@ -19,6 +19,10 @@ pub struct Axis2D<'a, 'b, T: 'a> {
     /// The height of the canvas that Axis2D should be plotted on
     height: f64,
 
+    /// The amount in pixels each tick on the axis should be seperated by
+    tick_amount_x: f64,
+    tick_amount_y: f64,
+
     options: AxisOptions<'b>,
 
     /// The canvas that the axis should be plotted on
@@ -28,7 +32,10 @@ pub struct Axis2D<'a, 'b, T: 'a> {
 impl<'a, 'b, T: Canvas> Axis2D<'a, 'b, T> {
     pub fn new(max_x: f64, max_y: f64, min_x: f64, min_y: f64, opts: AxisOptions<'b>, canvas: &'a mut T) -> Axis2D<'a, 'b, T> {
         let (w, h) = canvas.get_size();
-
+        
+        let (max_x, min_x, tick_x) = calculate_pretty_axis_values(max_x, min_x, opts.tick_count);
+        let (max_y, min_y, tick_y) = calculate_pretty_axis_values(max_y, min_y, opts.tick_count);
+        
         Axis2D {
             max_x: max_x,
             max_y: max_y,
@@ -38,6 +45,9 @@ impl<'a, 'b, T: Canvas> Axis2D<'a, 'b, T> {
 
             width: w,
             height: h,
+
+            tick_amount_x: tick_x,
+            tick_amount_y: tick_y,
 
             options: opts,
 
@@ -101,39 +111,18 @@ impl<'a, 'b, T: Canvas> Axis2D<'a, 'b, T> {
     }
 
     pub fn plot_axises(&mut self) {
-        let tick_count = self.options.tick_count;
+        // These need to be here, as self can't be borrowed immutably
+        // during the mutable borrow of draw_axis();
         let max_x = self.max_x; let min_x = self.min_x;
         let max_y = self.max_y; let min_y = self.min_y;
+        let height = self.height; let width = self.width;
+        let tick_x = self.tick_amount_x; let tick_y = self.tick_amount_y;
 
-        let (max_x, min_x, tick_x) = self.calculate_pretty_axis_values(max_x, min_x, tick_count);
-        self.max_x = max_x; self.min_x = min_x;
+        let is_x = true; let is_y = false;
+        self.draw_axis(is_x, is_y, min_x, max_x, height, tick_x);
 
-        let mut x = min_x;
-        while x <= max_x {
-            let pix = self.graph_coord_to_pixel((x, 0.0));
-            let tick_size = self.height * self.options.tick_size;
-            self.canvas.draw_line((pix.x, pix.y), (pix.x, pix.y-tick_size));
-
-            let number_offset = self.height * self.options.number_offset;
-            self.canvas.write_num(x, (pix.x, pix.y - number_offset));
-
-            x += tick_x;
-        }
-
-        let (max_y, min_y, tick_y) = self.calculate_pretty_axis_values(max_y, min_y, tick_count);
-        self.max_y = max_y; self.min_y = min_y;
-
-        let mut y = min_y;
-        while y <= max_y {
-            let pix = self.graph_coord_to_pixel((0.0, y));
-            let tick_size = self.width * 0.01;
-            self.canvas.draw_line((pix.x, pix.y), (pix.x-tick_size, pix.y));        
-
-            let number_offset = self.height * self.options.number_offset;
-            self.canvas.write_num(y, (pix.x - number_offset, pix.y));
-            
-            y += tick_y;
-        }
+        let is_x = false; let is_y = true;
+        self.draw_axis(is_x, is_y, min_y, max_y, width, tick_y);
 
         self.plot_line((min_x, 0.0), (max_x, 0.0));
         self.plot_line((0.0, min_y), (0.0, max_y));
@@ -142,20 +131,21 @@ impl<'a, 'b, T: Canvas> Axis2D<'a, 'b, T> {
         self.canvas.show();
     }
 
-    // returns the upper and lower limits and the increment size
-    fn calculate_pretty_axis_values(&self, max: f64, min: f64, tick_count: f64) -> (f64, f64, f64) {
-        let range = max - min;
-        let temp_step = range/(tick_count - 1.0);
+    pub fn draw_axis(&mut self, is_x: bool, is_y: bool, min_i: f64, max_i: f64, space: f64, tick: f64) {
+        let is_x = if is_x { 1.0 } else { 0.0 };
+        let is_y = if is_y { 1.0 } else { 0.0 };
+        let mut i = min_i;
+        while i <= max_i {
+            let pix = self.graph_coord_to_pixel((i*is_x, i*is_y));
+            
+            let tick_size = space * self.options.tick_size;
+            self.canvas.draw_line((pix.x, pix.y), (pix.x - (tick_size*is_y), pix.y - (tick_size*is_x)));
 
-        let mag = (temp_step.log10() - 1.0).floor();
-        let ten = 10.0_f64;
-        let mag_pow = ten.powf(mag);
-       
-        let step_size = (temp_step / mag_pow).ceil() * mag_pow;
-        let ll = step_size * (min/step_size).floor();
-        let ul = step_size * (max/step_size).ceil();
+            let number_offset = space * self.options.number_offset;
+            self.canvas.write_num(i, (pix.x - (number_offset * is_y), pix.y - (number_offset * is_x)));
 
-        (ul, ll, step_size)
+            i += tick;
+        }
     }
 
     fn write_label<P: Into<Pixel>>(&mut self, label: &str, loc: P) {
@@ -174,4 +164,20 @@ impl<'a, 'b, T: Canvas> Axis2D<'a, 'b, T> {
         let half_way = self.max_y / 2.0;
         self.write_label(y_label, (0.0, half_way));
     }
+}
+
+// returns the upper and lower limits and the increment size
+fn calculate_pretty_axis_values(max: f64, min: f64, tick_count: f64) -> (f64, f64, f64) {
+    let range = max - min;
+    let temp_step = range/(tick_count - 1.0);
+
+    let mag = (temp_step.log10() - 1.0).floor();
+    let ten = 10.0_f64;
+    let mag_pow = ten.powf(mag);
+    
+    let step_size = (temp_step / mag_pow).ceil() * mag_pow;
+    let ll = step_size * (min/step_size).floor();
+    let ul = step_size * (max/step_size).ceil();
+
+    (ul, ll, step_size)
 }
