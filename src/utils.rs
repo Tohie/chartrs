427ebae1799ae258c9707a059351f3407dd5a5f4
Tohie <scottdjwales@gmail.com>
2016-12-23 21:57:10 +0000

    Add tests for get_max_coords and get_min_coords

--- src/utils.rs
+++ src/utils.rs
@@ -37,3 +37,42 @@ pub fn pretty_axis_values(max: f64, min: f64, tick_count: f64) -> (f64, f64, f64
 
     (ul, ll, step_size)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pixel::GraphCoord;
+    use std::f64;
+
+    // if get_max_coord and get_min_coord are correct,
+    // then logically find_x_and_y_by_predicate must be as well
+    // if both fail then problem is likely in find_x_and_y_by_predicate
+
+    // Test negatives for get_max_coord and get_min_coord
+    #[test]
+    fn test_negative_coords() {
+        let coords = &[GraphCoord::new(-15.0, -20.0), GraphCoord::new(-30.0, -40.0)];
+
+        assert_eq!(get_max_coord(coords), GraphCoord::new(-15.0, -20.0));
+        assert_eq!(get_min_coord(coords), GraphCoord::new(-30.0, -40.0));
+    }
+
+    #[test]
+    fn test_positive_coords() {
+        let coords = &[GraphCoord::new(15.0, 20.0), GraphCoord::new(30.0, 40.0), GraphCoord::new(60.0, 10.0)];
+
+        assert_eq!(get_max_coord(coords), GraphCoord::new(60.0, 40.0));
+        assert_eq!(get_min_coord(coords), GraphCoord::new(15.0, 10.0));
+    }
+
+    // for the empty case, we should return the opposite extreme,
+    // i.e. get_max_coord should return (f64::MIN, f64::MIN)
+    // this way it will not affect anything
+    #[test]
+    fn test_empty_case() {
+        let coords = &[];
+
+        assert_eq!(get_max_coord(coords), GraphCoord::new(f64::MIN, f64::MIN));
+        assert_eq!(get_min_coord(coords), GraphCoord::new(f64::MAX, f64::MAX));
+    }
+}
