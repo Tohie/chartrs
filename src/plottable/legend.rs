2556a32f8d9e88c7a3c26f2f6fbb1144f14496ff
Tohie <scottdjwales@gmail.com>
2016-12-25 19:45:30 +0000

    Allow legend to handle graphs with large amounts of DataSets

--- src/plottable/legend.rs
+++ src/plottable/legend.rs
@@ -11,8 +11,13 @@ impl<'a> Plottable for Legend<'a> {
         let Pixel { x: top_x, y: top_y } = 
             bounds.convert_to_pixel((bounds.max.x, bounds.max.y)).expect("bounds.max should be on grid");
 
+        let count = self.0.iter().filter(|&ds| ds.options.name != "").collect::<Vec<_>>().len();
+
+        let row_height = bounds.height * 0.025;
+        let title_height = bounds.height * 0.0375;
+
         let width = bounds.width * 0.15;
-        let height = bounds.height * 0.1;
+        let height = title_height + (row_height * (count as f64));
 
         let x = top_x - width;
         let y = top_y - height;
@@ -26,21 +31,19 @@ impl<'a> Plottable for Legend<'a> {
         canvas.fill_rect((x, y), width, height)?;
 
         let inset_x = 0.05;
-        let mut inset_y = 0.75;
         let x = x + (inset_x * width);
-        canvas.write_text("Legend", (x, y + (inset_y * height)))?;
+        canvas.write_text("Legend", (x, y + (height - title_height)))?;
 
-        let y_spacing = 0.2;
+        let mut y = y + 3.0;
         for &ds in self.0.iter() {
-            inset_y = inset_y - y_spacing;
-            let half_y = y + ((inset_y + (y_spacing / 2.0)) * height) + 1.1;
-            let bottom_y = y + (inset_y * height);
+            let half_y = y + (row_height / 2.0);
             
             canvas.set_color(ds.choose_color());
             canvas.draw_line((x, half_y), (x + 20.0, half_y))?;
             
             canvas.set_color(Color(0, 0, 0));
-            canvas.write_text(ds.options.name, (x + 25.0, bottom_y))?;
+            canvas.write_text(ds.options.name, (x + 25.0, y))?;
+            y += row_height;
         }
 
         Ok(())  
